5d377e292b395f331d79fe1b9c9048beaf027523
William Whitaker <billydwhitaker@gmail.com>
2021-02-21 18:45:51 -0500

    adds first animations

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
     <aside class="secondary">hello world! my name is...</aside>
-    <h1 class="primary">William Whitaker</h1>
-    <div class="links">
-      <a href="#">resume</a>
-      <a href="#">contact</a>
-    </div>
+    <h1 class="primary">
+      <button @click="show = !show">William Whitaker</button>
+    </h1>
+    <transition name="slide-fade">
+      <div v-if="show" class="links">
+        <a href="#">resume</a>
+        <a href="#">contact</a>
+      </div>
+    </transition>
     <aside class="secondary">I am a...</aside>
     <h2 class="primary">developer</h2>
     <div class="links">
@@ -22,12 +26,18 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      show: false,
+    }
+  },
+}
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
+  margin: 35px auto 0;
   min-height: 100vh;
   display: grid;
   grid-template-columns: 35px auto 35px;
@@ -38,7 +48,7 @@ export default {}
 .primary {
   font-size: 2em;
   background-color: #fffff0;
-  grid-column: 1/3;
+  grid-column: 2/4;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -47,13 +57,28 @@ export default {}
 .secondary {
   font-size: 1em;
   background-color: #56b7eb;
-  grid-column: 2/4;
+  grid-column: 1/3;
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
 .links {
-  display: none;
+  grid-column: 2/4;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
