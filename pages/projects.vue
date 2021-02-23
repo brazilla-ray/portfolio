c88132d190ceeba8290f636d9e23a42c60b1d513
William Whitaker <billydwhitaker@gmail.com>
2021-02-23 11:26:59 -0500

    adds projects page

---
+++ pages/projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <page-header-vue></page-header-vue>
+    <h1>projects</h1>
+  </div>
+</template>
+
+<script>
+import PageHeaderVue from '~/components/PageHeader.vue'
+
+export default {
+  components: {
+    PageHeaderVue,
+  },
+}
+</script>
+
+<style scoped></style>
