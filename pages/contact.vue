13d705a069748cf9015bceeaf847f0d48135e5fe
William Whitaker <billydwhitaker@gmail.com>
2021-02-23 13:53:55 -0500

    improves styling on contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,11 +4,25 @@
     <h1 class="page-title">get in touch</h1>
     <form class="contact-form" @submit.prevent="sendEmail">
       <label class="form-input-label">Name</label>
-      <input class="form-input" type="text" name="user_name" />
+      <input
+        class="form-input"
+        type="text"
+        name="user_name"
+        placeholder="name"
+      />
       <label class="form-input-label">Email</label>
-      <input class="form-input" type="email" name="user_email" />
+      <input
+        class="form-input"
+        type="email"
+        name="user_email"
+        placeholder="email"
+      />
       <label class="form-input-label">Message</label>
-      <textarea class="form-input" name="message"></textarea>
+      <textarea
+        class="message-input"
+        name="message"
+        placeholder="type your message here"
+      ></textarea>
       <input type="submit" value="Send" />
     </form>
   </div>
@@ -48,13 +62,26 @@ export default {
 .contact-form {
   display: flex;
   flex-flow: column;
-  align-items: center;
+  align-items: flex-start;
 }
 
-.form-input {
-  width: 75%;
+.form-input,
+.message-input {
+  width: 100%;
   border: none;
-  height: 3em;
+  height: 5vh;
+  background-color: #00001f;
+  color: #fffff0;
+  border-bottom: 3px solid #fffff0;
+  font-size: 1em;
+  font-family: 'Alata', sans-serif;
+}
+
+.message-input {
+  height: 35vh;
+  border: none;
+  background-color: #fffff0;
+  color: #00001f;
 }
 
 .form-input-label {
