dbb09f29ac75ff9e72007317b57b5de8b9effd15
William Whitaker <billydwhitaker@gmail.com>
2021-03-11 19:37:55 -0500

    moves global styles to main.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -38,7 +38,7 @@ export default {
   ],
 
   styleResources: {
-    scss: ['./assets/css/_colors.scss'],
+    scss: ['~/assets/css/_colors.scss'],
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
