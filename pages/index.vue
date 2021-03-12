fb24395daf9f6d121ef6c8e21624339b1d3c3f2a
William Whitaker <billydwhitaker@gmail.com>
2021-03-12 03:50:15 -0500

    moves grid container to main.scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,28 @@
 <template>
-  <div class="container">
-    <div class="grid-container">
-      <NuxtLink to="/" class="grid-boxes with-text title">
-        <h1>William Whitaker</h1>
-      </NuxtLink>
-      <NuxtLink to="/projects" class="grid-boxes with-text projects">
-        <h2>projects</h2>
-      </NuxtLink>
-      <NuxtLink to="/about" class="grid-boxes with-text about">
-        <h2>about</h2>
-      </NuxtLink>
-      <NuxtLink to="/contact" class="grid-boxes with-text contact">
-        <h2>contact</h2>
-      </NuxtLink>
-    </div>
+  <div class="grid-container">
+    <NuxtLink to="/" class="grid-boxes with-text title">
+      <h1>William Whitaker</h1>
+    </NuxtLink>
+    <NuxtLink to="/projects" class="grid-boxes with-text projects">
+      <h2>projects</h2>
+    </NuxtLink>
+    <NuxtLink to="/about" class="grid-boxes with-text about">
+      <h2>about</h2>
+    </NuxtLink>
+    <NuxtLink to="/contact" class="grid-boxes with-text contact">
+      <h2>contact</h2>
+    </NuxtLink>
   </div>
 </template>
 
 <script></script>
 
 <style lang="scss" scoped>
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(8, 12.5vw);
-  grid-template-rows: repeat(8, 12.5vh);
-  overflow: hidden;
-}
-
 .grid-container::before {
   content: '';
   background-color: $warm-gray-trans;
-  grid-column: 1/9;
-  grid-row: 1;
+  grid-column: 1/17;
+  grid-row: 1/3;
   transform: skewX(315deg);
   transform-origin: bottom;
 }
@@ -57,8 +48,8 @@
 .title {
   color: $near-white;
   background-color: $warm-gray;
-  grid-column: 1/9;
-  grid-row: 2/4;
+  grid-column: 1/17;
+  grid-row: 3/8;
 }
 
 .title h1 {
@@ -68,15 +59,15 @@
 .projects {
   background-color: $blue;
   color: rgb(0, 0, 31);
-  grid-column: 5/9;
-  grid-row: 4/6;
+  grid-column: 10/17;
+  grid-row: 8/12;
 }
 
 .about {
   background-color: $near-white;
   color: rgb(0, 0, 31);
-  grid-column: 1/5;
-  grid-row: 4/6;
+  grid-column: 2/10;
+  grid-row: 8/12;
 }
 
 .about h2 {
@@ -86,56 +77,60 @@
 .contact {
   background-color: $yellow;
   color: rgb(0, 0, 31);
-  grid-column: 5/9;
-  grid-row: 6/8;
+  grid-column: 10/17;
+  grid-row: 12/16;
 }
 
 @media screen and (min-width: 568px) {
+  .grid-container::before {
+    grid-row: 1/4;
+  }
+
   .title {
-    grid-row: 2/5;
+    grid-row: 4/10;
   }
 
   .projects,
   .about,
   .contact {
-    grid-row: 5/9;
+    grid-row: 10/17;
   }
 
   .projects {
-    grid-column: 5/7;
+    grid-column: 9/13;
   }
 
   .contact {
-    grid-column: 7/9;
+    grid-column: 13/17;
   }
 
   .about {
-    grid-column: 3/5;
+    grid-column: 5/9;
   }
 }
 
 @media screen and (min-width: 768px) {
   .title {
-    grid-column: 1/9;
-    grid-row: 2/4;
+    grid-column: 1/17;
+    grid-row: 4/8;
   }
 
   .about,
   .projects {
-    grid-row: 4/6;
+    grid-row: 8/12;
   }
 
   .about {
-    grid-column: 3/6;
+    grid-column: 5/11;
   }
 
   .projects {
-    grid-column: 6/9;
+    grid-column: 11/17;
   }
 
   .contact {
-    grid-column: 6/9;
-    grid-row: 6/8;
+    grid-column: 11/17;
+    grid-row: 12/16;
   }
 
   .with-text h1,
@@ -151,7 +146,7 @@
 
 @media screen and (min-width: 1024px) {
   .grid-container::before {
-    grid-column: 2/7;
+    grid-column: 2/13;
   }
 
   .title::before {
@@ -160,31 +155,31 @@
     position: absolute;
     top: 0;
     right: 12.5vw;
-    height: 12.5vh;
+    height: 18.75vh;
     width: 12.5vw;
     background-color: $warm-gray-dark;
   }
 
   .title {
-    grid-column: 2/8;
+    grid-column: 2/15;
   }
 
   .about {
-    grid-column: 4/6;
+    grid-column: 7/11;
   }
 
   .about,
   .projects {
-    grid-row: 4/7;
+    grid-row: 8/14;
   }
 
   .projects {
-    grid-column: 6/8;
+    grid-column: 11/15;
   }
 
   .contact {
-    grid-column: 4/8;
-    grid-row: 7/9;
+    grid-column: 7/15;
+    grid-row: 14/17;
   }
 }
 </style>
