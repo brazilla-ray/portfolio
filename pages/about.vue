961e9b0fac58ab67d02049445938dfdbaf0d1634
William Whitaker <billydwhitaker@gmail.com>
2021-03-16 15:07:46 -0400

    adds photo to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="grid-container">
     <page-header-vue class="page-header"></page-header-vue>
+    <div class="page-title">
+      <h1>about me</h1>
+    </div>
     <div class="content-wrapper">
-      <div class="page-title">
-        <h1>about me</h1>
-      </div>
       <article class="main-content">
+        <img src="~/static/self.png" alt="self-portrait" class="inline-image" />
         <nuxt-content :document="page" />
       </article>
     </div>
@@ -38,5 +39,17 @@ export default {
 
 .nuxt-content > p > a {
   color: $blue;
+}
+
+.inline-image {
+  max-width: 31.25vw;
+  margin: 0 1em 1em 0;
+  float: left;
+}
+
+@media screen and (min-width: 568px) {
+  .inline-image {
+    margin: 0 2em 2em 0;
+  }
 }
 </style>
