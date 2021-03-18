130a4f889eb74b95baf43a112f9ea0186b534bcd
William Whitaker <billydwhitaker@gmail.com>
2021-03-17 20:56:38 -0400

    updates projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -5,101 +5,103 @@
       <h1>projects</h1>
     </div>
     <div class="content-wrapper">
-      <div class="main-content">
+      <article class="main-content">
         <section>
-          <h2 class="section-title">my website over the years...</h2>
-          <article class="section-content">
-            <p>
-              I've made several websites to showcase my artwork. This first was
-              just a simple, static HTML site. There was a tiny bit of
-              Javascript in there too.
-            </p>
+          <h2>art</h2>
+          <br />
+          <p>
+            The first site I built for my artwork was pretty basic: HTML, CSS,
+            and a little JavaScript. I went on to build sites with Drupal, Ruby
+            on Rails, and WordPress. There may have been easier ways to go about
+            it, but I've always liked making things from scratch.
+          </p>
+          <br />
+          <figure>
             <a href="https://brazilla-ray.github.io/first-site/">
               <img
+                src="~/static/first_site.png"
                 class="screenshot"
-                src="~/static/JPEG/first_site.jpg"
-                alt="my first website"
+                alt="My first website"
               />
             </a>
-            <p>
-              I quickly realized two things: first, I loved doing this, and
-              second, that I would have to update things by hand. Which led me
-              to Drupal. I coded this one during a blizzard.
-            </p>
+            <figcaption>
+              <aside>My first site.</aside>
+              <code>HTML, CSS, JavaScript</code>
+            </figcaption>
+          </figure>
+          <figure>
             <img
+              src="~/static/mrww_v1.png"
               class="screenshot"
-              src="~/static/JPEG/mrww_v1.jpg"
-              alt="drupal site version 1"
+              alt="mrwilliamwhitaker.com screenshot"
             />
-            <p>
-              This is another version, where I tried my hand at building a theme
-              from scratch.
-            </p>
+            <figcaption>
+              <aside>mrwilliamwhitaker.com, c.2011</aside>
+              <code>Drupal</code>
+            </figcaption>
+          </figure>
+          <figure>
             <img
+              src="~/static/mrww_v2.png"
               class="screenshot"
-              src="~/static/JPEG/mrww_v2.jpg"
-              alt="drupal site 2013"
+              alt="mrwilliamwhitaker.com screenshot"
             />
-            <p>At one point, I decided to learn Ruby on Rails.</p>
+            <figcaption>
+              <aside>c.2013</aside>
+              <code>Drupal</code>
+            </figcaption>
+          </figure>
+          <figure>
             <img
+              src="~/static/mrww_v3.png"
               class="screenshot"
-              src="~/static/JPEG/mrww_v3.jpg"
-              alt="rails site 2014"
+              alt="mrwilliamwhitaker.com screenshot"
             />
-            <p>
-              This is the current version of my site. It's a WordPress site,
-              with a custom theme I coded.
-            </p>
-            <a href="https://williamwhitaker.art">
-              <img
-                class="screenshot"
-                src="~/static/JPEG/current_site.jpg"
-                alt="current site"
-              />
-            </a>
-            <p>
-              I'm currently working on a
-              <a href="https://pensive-aryabhata-8caa68.netlify.app/"
-                >version</a
-              >
-              that uses WordPress as a headless CMS, with a Nuxt app on the
-              frontend. It should be finished soon!
-            </p>
-          </article>
+            <figcaption>
+              <aside>c.2015</aside>
+              <code>Ruby on Rails</code>
+            </figcaption>
+          </figure>
+          <br />
+          <p>
+            Last summer, I switched my domain to
+            <a href="https://williamwhitaker.art">williamwhitaker.art</a>
+            and coded my first WordPress
+            <a href="https://github.com/brazilla-ray/ww-theme">theme</a>, based
+            on <a href="https://underscores.me/">_s</a>, as well as my first
+            <a href="https://github.com/brazilla-ray/ww-plugin">plugin.</a>
+          </p>
+          <br />
+          <p>
+            I'm currently working on a
+            <a href="https://github.com/brazilla-ray/wwa-client">version</a>
+            that uses WordPress as a headless CMS, with a Nuxt app as the
+            frontend. You can preview it
+            <a href="https://pensive-aryabhata-8caa68.netlify.app/">here</a>.
+          </p>
         </section>
         <section>
-          <h2 class="section-title">some other projects...</h2>
-          <article class="section-content">
-            <p>
-              I made an Angular
-              <a href="https://brazilla-ray.github.io/sarApp/">app</a> for my
-              sister, who's a published
-              <a
-                href="https://www.amazon.com/What-Comes-Dreams-Sarah-Robinson/dp/1480873233/ref=sr_1_1?dchild=1&keywords=what+comes+of+dreams+sarah+robinson&qid=1614222591&sr=8-1"
-                >author!</a
-              >
-            </p>
-            <img
-              class="screenshot"
-              src="~/static/JPEG/sar_sketch.jpg"
-              alt="sarApp sketch"
-            />
-            <p>I really need to get back to work on that one.</p>
-            <p>
-              Last but not least is the
-              <a href="https://brazilla-ray.github.io/wedding-site/">site</a> I
-              made for my wedding in 2016. I built it using Yeoman and Gulp. I
-              had a lot of trouble with getting the form working, but a friend
-              helped me out.
-            </p>
-            <img
-              class="screenshot"
-              src="~/static/JPEG/wedding_site.jpg"
-              alt="wedding site"
-            />
-          </article>
+          <br />
+          <h2>other things</h2>
+          <br />
+          <p>
+            When I got married, I built the website for our wedding.
+            <a href="https://brazilla-ray.github.io/wedding-site/">It</a>
+            handled the RSVPs and menu choice of the guests.
+          </p>
+          <p>
+            You can check out some of the things I've made on
+            <a href="https://codepen.io/w0whitaker">CodePen</a>, including
+            <a href="https://codepen.io/collection/DKwbKa">this</a> collection
+            that was a picked pen a while back.
+          </p>
+          <br />
+          <figure>
+            <img src="~/static/codePen.png" alt="CodePen" class="screenshot" />
+            <aside>Exciting!</aside>
+          </figure>
         </section>
-      </div>
+      </article>
     </div>
   </div>
 </template>
@@ -108,9 +110,7 @@
 import PageHeaderVue from '~/components/PageHeader.vue'
 
 export default {
-  components: {
-    PageHeaderVue,
-  },
+  components: { PageHeaderVue },
 }
 </script>
 
@@ -130,9 +130,6 @@ article > p > a {
 
 .screenshot {
   width: 250px;
-  margin-top: 35px;
-  border-radius: 5%;
-  border: 3px solid #56b7eb;
   padding: 3px;
 }
 </style>
