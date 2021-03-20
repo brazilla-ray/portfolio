71a3872c42f4f2ae30c31981ccad5ba61ff8846c
William Whitaker <billydwhitaker@gmail.com>
2021-03-20 16:44:50 -0400

    adds margin to main nav items

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -1,8 +1,14 @@
 <template>
   <nav class="flex-container flex-column mt-1">
-    <NuxtLink to="/about">about</NuxtLink>
-    <NuxtLink to="/projects">projects</NuxtLink>
-    <NuxtLink to="/contact">contact</NuxtLink>
+    <div class="mt-1">
+      <NuxtLink to="/about">about</NuxtLink>
+    </div>
+    <div class="mt-1">
+      <NuxtLink to="/projects">projects</NuxtLink>
+    </div>
+    <div class="mt-1">
+      <NuxtLink to="/contact">contact</NuxtLink>
+    </div>
   </nav>
 </template>
 
