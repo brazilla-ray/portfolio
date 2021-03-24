684fdc2e114cb32f6f0b7b7c3882ef72535f7c19
William Whitaker <billydwhitaker@gmail.com>
2021-03-24 11:43:20 -0400

    moves link markup to component

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -1,64 +1,17 @@
 <template>
   <nav class="mt-1">
-    <NuxtLink
-      v-if="currentPageName === 'about' || currentPageName === 'index'"
-      to="/about"
-      class="flex-container flex-centered box about section-title mt-1"
-    >
-      <h2>about</h2>
-    </NuxtLink>
-    <div v-else class="flex-container flex-centered box mt-1">
-      <NuxtLink to="/about" class="link">about</NuxtLink>
-    </div>
-    <NuxtLink
-      v-if="currentPageName === 'projects' || currentPageName === 'index'"
-      to="/projects"
-      class="flex-container flex-centered box projects section-title mt-1"
-    >
-      <h2>projects</h2>
-    </NuxtLink>
-    <div v-else class="flex-container flex-centered box mt-1">
-      <NuxtLink to="/projects" class="link">projects</NuxtLink>
-    </div>
-    <NuxtLink
-      v-if="currentPageName === 'contact' || currentPageName === 'index'"
-      to="/contact"
-      class="flex-container flex-centered box contact section-title mt-1"
-    >
-      <h2>contact</h2>
-    </NuxtLink>
-    <div v-else class="flex-container flex-centered box mt-1">
-      <NuxtLink to="/contact" class="link">contact</NuxtLink>
-    </div>
+    <nav-item-vue page-name="about"></nav-item-vue>
+    <nav-item-vue page-name="projects"></nav-item-vue>
+    <nav-item-vue page-name="contact"></nav-item-vue>
   </nav>
 </template>
 
 <script>
+import NavItemVue from '~/components/NavItem.vue'
+
 export default {
-  computed: {
-    currentPageName() {
-      return this.$route.name
-    },
-  },
+  components: { NavItemVue },
 }
 </script>
 
-<style lang="scss" scoped>
-.box {
-  width: 150px;
-  height: 150px;
-  padding-left: 1rem;
-}
-
-.about {
-  background-color: $near-white;
-}
-
-.projects {
-  background-color: $blue;
-}
-
-.contact {
-  background-color: $yellow;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ components/NavItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div
+      v-if="currentPageName === pageName || currentPageName === 'index'"
+      :class="[pageName, classList]"
+    >
+      <NuxtLink :to="{ path: pageName }">
+        <h2 class="section-title">{{ pageName }}</h2>
+      </NuxtLink>
+    </div>
+    <div v-else :class="classList">
+      <NuxtLink :to="{ path: pageName }" class="link">{{ pageName }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageName: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      classList: ['flex-container', 'flex-centered', 'box', 'mt-1'],
+    }
+  },
+  computed: {
+    currentPageName() {
+      return this.$route.name
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 150px;
+  height: 150px;
+  padding-left: 1rem;
+}
+.about {
+  background-color: $near-white;
+}
+
+.projects {
+  background-color: $blue;
+}
+
+.contact {
+  background-color: $yellow;
+}
+</style>
