5390e2b9e64c9f8c822a76654cbf95d05ae79f12
William Whitaker <billydwhitaker@gmail.com>
2021-03-24 20:24:59 -0400

    sets up secondary menu

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,20 @@
     <div class="full-width">
       <main-nav-vue></main-nav-vue>
     </div>
+    <div class="secondary-nav">
+      <nav class="flex-container flex-column">
+        <a href="https://twitter.com/w0whitaker" class="link">twitter</a>
+        <a href="https://github.com/brazilla-ray" class="link mt-2">github</a>
+        <a href="https://codepen.io/w0whitaker" class="link mt-2">codepen</a>
+        <a
+          href="https://www.linkedin.com/in/william-whitaker-4230bbab/"
+          class="link mt-2"
+        >
+          linkedin</a
+        >
+        <a href="https://williamwhitaker.art" class="link mt-2">art</a>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -25,4 +39,10 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.secondary-nav {
+  margin-top: -1.5rem;
+  grid-column: 12;
+  grid-row: 3;
+}
+</style>
