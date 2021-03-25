801b1d595aa438d39290569d30dca3024049d371
William Whitaker <billydwhitaker@gmail.com>
2021-03-25 04:59:13 -0400

    changes text-layout for main content

--- pages/about.vue
+++ pages/about.vue
@@ -6,6 +6,14 @@
     <div class="full-width">
       <main-nav-vue></main-nav-vue>
     </div>
+    <div class="content-wrapper mt-1 normal light">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ducimus
+        praesentium totam consequuntur molestias, nemo molestiae a asperiores
+        eius quam earum amet incidunt sed explicabo, iste ex temporibus dolorum
+        sint!
+      </p>
+    </div>
   </div>
 </template>
 
@@ -25,4 +33,14 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content-wrapper {
+  grid-column: 10/-1;
+  grid-row: 2/-1;
+  background-color: $near-white-quarter-trans;
+  margin-right: -1em;
+  padding: 1rem 2rem;
+  border-left: 6px solid $near-black;
+  z-index: -1;
+}
+</style>
