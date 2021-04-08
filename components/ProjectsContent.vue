9424c9836e22725f1c203520ba727d547a7e06af
William Whitaker <billydwhitaker@gmail.com>
2021-04-08 19:24:00 -0400

    finishes content for projects page

--- components/ProjectsContent.vue
+++ components/ProjectsContent.vue
@@ -1,11 +1,91 @@
 <template>
-  <div>
-    <h2>ohai!</h2>
-  </div>
+  <article>
+    <section>
+      <h3>mrwilliamwhitaker.com</h3>
+      <ul class="mt-1">
+        <li>
+          <p>
+            the
+            <a href="https://brazilla-ray.github.io/first-site/" class="link"
+              >first</a
+            >
+            website I made was just:
+          </p>
+          <ul class="ml-1">
+            <li>html</li>
+            <li>&amp; css</li>
+          </ul>
+        </li>
+        <li class="mt-1">
+          <p>I've since made versions of it using:</p>
+          <ul class="ml-1">
+            <li>drupal</li>
+            <li>ruby on rails</li>
+            <li>&amp; wordpress</li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+    <section class="mt-1">
+      <h3>williamwhitaker.art</h3>
+      <ul class="mt-1">
+        <li class="mt-1">
+          <p>
+            my
+            <a href="https://williamwhitaker.art" class="link">current</a> site:
+          </p>
+          <ul class="ml-1">
+            <li>wordpress</li>
+            <li>custom theme</li>
+          </ul>
+        </li>
+        <li class="mt-1">
+          <p>
+            in
+            <a href="https://pensive-aryabhata-8caa68.netlify.app/" class="link"
+              >development</a
+            >:
+          </p>
+          <ul class="ml-1">
+            <li>headless cms (wordpress)</li>
+            <li>nuxt/vue</li>
+            <li>netlify</li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+    <section class="mt-1">
+      <h3>other projects</h3>
+      <p class="mt-1">other things I've worked on can be found over at:</p>
+      <ul class="ml-1">
+        <li>
+          <a href="https://github.com/brazilla-ray" class="link">github</a>
+        </li>
+        <li>
+          <a href="https://codepen.io/w0whitaker" class="link">&amp; codepen</a>
+        </li>
+      </ul>
+    </section>
+  </article>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h3 {
+  font-size: 1em;
+  color: $green;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+img {
+  max-width: 150px;
+}
+</style>
