b1139277dab47e4b1a744dbf161e880f359ca28a
William Whitaker <billydwhitaker@gmail.com>
2021-04-08 19:48:20 -0400

    removes secondary nav menu from index

--- pages/index.vue
+++ pages/index.vue
@@ -8,20 +8,6 @@
     <div class="full-width row-2">
       <main-nav-vue></main-nav-vue>
     </div>
-    <div class="secondary-nav row-2 col-13">
-      <nav class="flex-container flex-column">
-        <a href="https://twitter.com/w0whitaker" class="link">twitter</a>
-        <a href="https://github.com/brazilla-ray" class="link mt-2">github</a>
-        <a href="https://codepen.io/w0whitaker" class="link mt-2">codepen</a>
-        <a
-          href="https://www.linkedin.com/in/william-whitaker-4230bbab/"
-          class="link mt-2"
-        >
-          linkedin</a
-        >
-        <a href="https://williamwhitaker.art" class="link mt-2">art</a>
-      </nav>
-    </div>
   </div>
 </template>
 
@@ -36,8 +22,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.secondary-nav {
-  margin-top: 6.75rem;
-}
-</style>
+<style lang="scss" scoped></style>
