c172fc09f782d34fcc63da634525d5bb2bf393a0
William Whitaker <billydwhitaker@gmail.com>
2021-04-08 19:46:33 -0400

    finishes contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,14 +3,13 @@
     <div class="indented">
       <header-vue site-title="ww"></header-vue>
     </div>
-    <div class="full-width">
+    <div class="full-width row-2">
       <main-nav-vue></main-nav-vue>
     </div>
-    <div class="content behind">
-      <main-content-vue
-        page-style="contact-content"
-        class="mt-1"
-      ></main-content-vue>
+    <div class="content row-2">
+      <main-content-vue page-style="contact-content">
+        <contact-form-vue></contact-form-vue>
+      </main-content-vue>
     </div>
   </div>
 </template>
@@ -20,12 +19,14 @@ import emailjs from 'emailjs-com'
 import HeaderVue from '~/components/Header.vue'
 import MainNavVue from '~/components/MainNav.vue'
 import MainContentVue from '~/components/MainContent.vue'
+import ContactFormVue from '~/components/ContactForm.vue'
 
 export default {
   components: {
     HeaderVue,
     MainNavVue,
     MainContentVue,
+    ContactFormVue,
   },
   methods: {
     sendEmail: (e) => {
@@ -49,49 +50,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.grid-container::after {
-  background-color: $yellow;
-  grid-column: 10/17;
-}
-
-.contact-form {
-  display: flex;
-  flex-flow: column;
-  align-items: flex-start;
-}
-
-.form-input,
-.message-input {
-  width: 100%;
-  border: none;
-  height: 5vh;
-  background-color: $near-black;
-  color: $near-white;
-  border-bottom: 3px solid $near-white;
-  font-size: 1em;
-  font-family: 'Alata', sans-serif;
-}
-
-.form-input-label {
-  margin: 2em 0 1em;
-  color: #56b7eb;
-}
-
-.message-input {
-  height: 35vh;
-  border: none;
-  background-color: $near-white;
-  color: $near-black;
-}
-
-.message-submit {
-  border: 3px solid #56b7eb;
-  font-size: 2em;
-  color: #56b7eb;
-  background-color: $near-black;
-  font-family: 'Alata', sans-serif;
-  margin-top: 17.5px;
-  border-radius: 10%;
-}
-</style>
+<style lang="scss" scoped></style>
